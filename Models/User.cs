66216d498f79a172671743c99106734d9b8f8a96
braianpx <braivaldez16@gmail.com>
2026-02-11 03:44:47 -0300

    Agregar archivos de proyecto.

---
+++ Models/User.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Zoco.Api.Models
+{
+    [Index(nameof(Email), IsUnique = true)]
+
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Role { get; set; } // Puede ser Admin o User
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        //Relaciones
+    }
+}
