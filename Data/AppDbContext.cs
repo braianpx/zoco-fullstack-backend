593438e45e67a35d47925c920139c06956a92c12
braianpx <braivaldez16@gmail.com>
2026-02-12 00:38:15 -0300

    feat: agregar DbSets y configuracion de modelos en AppDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Zoco.Api.Models;
 
 namespace Zoco.Api.Data
 {
@@ -7,6 +8,62 @@ namespace Zoco.Api.Data
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
+        }
+        //DbSets
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Role> Roles => Set<Role>();
+        public DbSet<Study> Studies => Set<Study>();
+        public DbSet<Address> Addresses => Set<Address>();
+        public DbSet<SessionLog> SessionLogs => Set<SessionLog>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // ==============================
+            // CONFIGURACIÓN
+            // ===================
+
+            // ROLE
+            modelBuilder.Entity<Role>()
+                .HasIndex(r => r.Name)
+                .IsUnique();
+            // Crea automaticamente los roles
+            modelBuilder.Entity<Role>().HasData(
+               new Role { Id = 1, Name = "Admin" },
+               new Role { Id = 2, Name = "User" }
+           );
+
+            //USER
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Role)
+                .WithMany(r => r.Users)
+                .HasForeignKey(u => u.RoleId)
+                .OnDelete(DeleteBehavior.Restrict); // no elimina los roles cuando se borra un usuario
+
+            //STUDY
+            modelBuilder.Entity<Study>()
+               .HasOne(s => s.User)
+               .WithMany(u => u.Studies)
+               .HasForeignKey(s => s.UserId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+            //ADDRESS
+            modelBuilder.Entity<Address>()
+                .HasOne(a => a.User)
+                .WithMany(u => u.Addresses)
+                .HasForeignKey(a => a.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            //SESSION LOG
+            modelBuilder.Entity<SessionLog>()
+                .HasOne(sl => sl.User)
+                .WithMany(u => u.SessionLogs)
+                .HasForeignKey(sl => sl.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
         }
 
 
