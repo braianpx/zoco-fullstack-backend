c937930b1ccf9955adaf7298383ec91c102d0499
braianpx <braivaldez16@gmail.com>
2026-02-12 04:11:19 -0300

    feat: agregar midleware para excepciones y manejo de errores global

---
+++ Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Zoco.Api.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context); // Ejecuta el siguiente middleware / controller
+            }
+            catch (Exception Err)
+            {
+                _logger.LogError(Err.ToString(), "Ocurrió un error inesperado");
+
+                context.Response.StatusCode = 500;
+                context.Response.ContentType = "application/json";
+
+                await context.Response.WriteAsJsonAsync(new
+                {
+                    message = "Ocurrió un error inesperado en el servidor",
+                    detail = Err.Message
+                });
+            }
+        }
+    }
+}
