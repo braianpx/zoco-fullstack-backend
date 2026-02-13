7d4628ae4f2d4d3b1dd52188dfb6edcaf5688745
braianpx <braivaldez16@gmail.com>
2026-02-13 06:06:19 -0300

    feat: agreagar configs de validacion de datos y claims

---
+++ Extensions/ApiBehaviorExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Zoco.Api.Models.DTOs;
+
+namespace Zoco.Api.Extensions
+{
+    public static class ApiBehaviorExtensions
+    {
+        public static IServiceCollection AddCustomApiBehavior(
+            this IServiceCollection services)
+        {
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = context =>
+                {
+                    var errors = context.ModelState.Values
+                        .SelectMany(v => v.Errors)
+                        .Select(e => e.ErrorMessage)
+                        .ToList();
+
+                    var response = new ApiResponse<List<string>>
+                    {
+                        Success = false,
+                        Message = "Errores de validación",
+                        Data = errors
+                    };
+
+                    return new BadRequestObjectResult(response);
+                };
+            });
+
+            return services;
+        }
+    }
+}
---
+++ Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static int GetUserId(this ClaimsPrincipal user)
+    {
+        return int.Parse(
+            user.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+    }
+
+    public static string GetUserRole(this ClaimsPrincipal user)
+    {
+        return user.FindFirst(ClaimTypes.Role)!.Value;
+    }
+}
