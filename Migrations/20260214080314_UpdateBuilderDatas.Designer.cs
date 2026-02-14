c31291969106b89a58e6c9311150c9d3bc3e640e
braianpx <braivaldez16@gmail.com>
2026-02-14 06:09:33 -0300

    feat: agregar nuevas migraciones de datos

--- Migrations/20260214080314_UpdateBuilderDatas.Designer.cs
+++ Migrations/20260214080314_UpdateBuilderDatas.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Zoco.Api.Data;
 
@@ -11,9 +12,11 @@ using Zoco.Api.Data;
 namespace Zoco.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260214080314_TestMigration")]
+    partial class TestMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -56,7 +59,7 @@ namespace Zoco.Api.Migrations
 
                     b.HasIndex("UserId");
 
-                    b.ToTable("Addresses", (string)null);
+                    b.ToTable("Addresses");
                 });
 
             modelBuilder.Entity("Zoco.Api.Models.Role", b =>
@@ -74,7 +77,7 @@ namespace Zoco.Api.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Roles", (string)null);
+                    b.ToTable("Roles");
                 });
 
             modelBuilder.Entity("Zoco.Api.Models.SessionLog", b =>
@@ -98,7 +101,7 @@ namespace Zoco.Api.Migrations
 
                     b.HasIndex("UserId");
 
-                    b.ToTable("SessionLogs", (string)null);
+                    b.ToTable("SessionLogs");
                 });
 
             modelBuilder.Entity("Zoco.Api.Models.Study", b =>
@@ -132,7 +135,7 @@ namespace Zoco.Api.Migrations
 
                     b.HasIndex("UserId");
 
-                    b.ToTable("Studies", (string)null);
+                    b.ToTable("Studies");
                 });
 
             modelBuilder.Entity("Zoco.Api.Models.User", b =>
@@ -175,7 +178,7 @@ namespace Zoco.Api.Migrations
 
                     b.HasIndex("RoleId");
 
-                    b.ToTable("Users", (string)null);
+                    b.ToTable("Users");
                 });
 
             modelBuilder.Entity("Zoco.Api.Models.Address", b =>
---
+++ Migrations/20260214080314_UpdateBuilderDatas.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Zoco.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Roles_Name",
+                table: "Roles");
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Admin" },
+                    { 2, "User" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_Name",
+                table: "Roles",
+                column: "Name",
+                unique: true);
+        }
+    }
+}
