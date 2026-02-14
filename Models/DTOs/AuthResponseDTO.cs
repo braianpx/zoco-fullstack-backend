88580f427e9961adf01e991bb1c9e4e83a00d9fa
braianpx <braivaldez16@gmail.com>
2026-02-14 06:13:17 -0300

    refactory: agregar role del usuario a la respuesta del logeo

--- Models/DTOs/AuthResponseDTO.cs
+++ Models/DTOs/AuthResponseDTO.cs
@@ -4,5 +4,6 @@
     {
         public string Token { get; set; } = string.Empty;
         public DateTime Expiration { get; set; }
+        public string Role { get; set; } = string.Empty;
     }
 }
