b54701784baf6ac69da562608dcbc6bdee479215
braianpx <braivaldez16@gmail.com>
2026-02-15 18:11:17 -0300

    feat: agregar cors

---
+++ Extensions/CorsServiceExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Zoco.Api.Extensions
+{
+    public static class CorsServiceExtensions
+    {
+        public static IServiceCollection AddCustomCors(this IServiceCollection services,
+            IConfiguration configuration, string policyName)
+        {
+            var allowedOrigins = configuration.GetSection("CorsSettings:AllowedOrigins").Get<string[]>();
+
+            return services.AddCors(options =>
+            {
+                options.AddPolicy(name: policyName,
+                    policy =>
+                    {
+                        if (allowedOrigins != null && allowedOrigins.Any())
+                        {
+                            policy.WithOrigins(allowedOrigins) // Carga el array del JSON
+                                  .AllowAnyHeader()
+                                  .AllowAnyMethod()
+                                  .AllowCredentials(); // Agrega esto si usas cookies/Identity
+                        }
+                    });
+            });
+        }
+    }
+}
